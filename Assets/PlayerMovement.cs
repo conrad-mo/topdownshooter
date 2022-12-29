831341a10610fa7bad77da6f4a2dfbe399bee66d
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2022-12-29 16:49:04 -0500

    Initial Commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 100f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = 0;
+        movement.y = 0;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            movement.x = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movement.x = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movement.y = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movement.y = -1;
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //movement.x = 0 * Time.fixedDeltaTime;
+        //movement.y = 0 * Time.fixedDeltaTime;
+    }
+}

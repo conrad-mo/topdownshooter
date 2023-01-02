1677968989a0a8779467e19717b6171b23cb70c5
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2023-01-02 17:24:06 -0500

    Fix player targetting

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    public float followspeed = 0.1f;
+    private float followspeed = 2f;
     public GameObject player;
+    public GameObject enemy;
     public Rigidbody2D rb;
     Vector2 movement;
     Vector2 playerPos;
     // Update is called once per frame
     void Start()
     {
-        
+       // Instantiate(enemy, new Vector3(2, 2), transform.rotation);
     }
     void Update()
     {
@@ -20,7 +21,7 @@ public class EnemyScript : MonoBehaviour
     }
     void FixedUpdate()
     {
-        rb.velocity = Vector2.up * followspeed;
+        rb.velocity = transform.up * followspeed;
         Vector2 lookDir = playerPos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;

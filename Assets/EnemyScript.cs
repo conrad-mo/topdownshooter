10ae02c38ed534962b40bed15c3c63d73fe9054a
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2023-01-03 22:01:35 -0500

    Holy fuck I am the biggest bozo

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -33,7 +33,7 @@ public class EnemyScript : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(gameObject);
         }
